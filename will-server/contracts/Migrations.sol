ed662f89028b70fc84cfd4f05c595de01cb1dcab
BGlar <bogolla4@gmail.com>
2021-03-15 17:58:50 +0200

    Add contracts; Owned and DocumentStorage

--- will-server/contracts/Migrations.sol
+++ will-server/contracts/Migrations.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.5.16 <0.8.0;
 
 contract Migrations {
   address public owner = msg.sender;
@@ -15,5 +15,10 @@ contract Migrations {
 
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
+  }
+
+  function upgrade(address new_address) public restricted {
+    Migrations upgraded = Migrations(new_address);
+    upgraded.setCompleted(last_completed_migration);
   }
 }
